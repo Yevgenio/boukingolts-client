5a65fda8e9d89485e05c1e11c6cd1fe0cc561196
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2024-12-07 20:26:51 +0200

    api address fix

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
     // apiBaseUrl: 'http://127.0.0.1', //localhost
     // apiBaseUrl: 'http://51.84.9.120', // my server
     // apiBaseUrl: 'http://51.16.37.75', // student 7 aws
-    apiBaseUrl: 'student-7.boukingolts.art',
+    apiBaseUrl: 'boukingolts.art',
     defaultLanguage: 'he',
     otherGlobalVariable: 'value',
   };
