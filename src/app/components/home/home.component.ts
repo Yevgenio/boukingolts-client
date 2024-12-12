1bc543c83c5eaafaf3ae27ea9d7cc758773045d6
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2024-12-12 13:04:33 +0200

    fixing build error 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home', // ensure this matches your routing configuration
@@ -9,15 +8,6 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./home.component.scss'],
   standalone: true,
   imports: [CommonModule, RouterModule],
-  animations: [
-    trigger('fadeInUp', [
-      state('void', style({ opacity: 0, transform: 'translateY(20px)' })),
-      state('*', style({ opacity: 1, transform: 'translateY(0)' })),
-      transition('void => *', [
-        animate('600ms cubic-bezier(0.25, 0.8, 0.25, 1)')
-      ])
-    ])
-  ]
 })
 
 export class HomeComponent {
