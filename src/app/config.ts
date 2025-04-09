652fb8b665e105bfa5f2419a54354def3744a980
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-04-10 00:37:56 +0300

    api url change

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,7 @@
 export const CONFIG = {
-    apiBaseUrl: 'http://localhost:5000', //localhost
-    // apiBaseUrl: 'http://51.84.9.120', // my server
-    // apiBaseUrl: 'http://51.16.37.75', // student 7 aws
+    // apiBaseUrl: 'http://localhost:5000', //localhost
     // apiBaseUrl: 'https://student-7.boukingolts.art',
-    // apiBaseUrl: '',
+    apiBaseUrl: '', // production
     defaultLanguage: 'he',
     otherGlobalVariable: 'value',
   };
